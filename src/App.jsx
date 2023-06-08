9efa7ba1864b64787459a90aca0aa15df24a0449
Cristian Hernandez <chernandezz@gmail.com>
2023-06-08 09:19:06 -0700

    Se dio la estructura inicial del APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Header from "./UI/Header";
+import Reserva from "./Components/Reserva";
+import Proyectos from "./Components/Proyectos";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Reserva />
+      <Proyectos />
+    </>
+  );
 }
 
 export default App;
